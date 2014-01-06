22234a4e6fa0b383274cb8ae0ab1588191929514
zzWinD_company <gongqizhou@qq.com>
2014-01-06 06:29:20 -0200

    Temp Up

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,9 @@ class Product < ActiveRecord::Base
     with: %r{\.(gif|jpg|png)\Z}i,
     message: 'must be a URL for GIF, JPG or PNG image.'
   }
+
+
+  def self.latest
+    Product.order(:update_at).last
+  end
 end
