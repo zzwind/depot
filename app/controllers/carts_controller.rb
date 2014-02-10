e2ee3b024185a0ca16110e69b0cbe925cd6112b8
zzWinD_company <gongqizhou@qq.com>
2014-02-10 05:59:24 -0200

    ajax

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -56,9 +56,11 @@ class CartsController < ApplicationController
   # DELETE /carts/1.json
   def destroy
     @cart.destroy
+    session[:cart_id] = nil
     respond_to do |format|
-      format.html { redirect_to carts_url }
-      format.json { head :no_content }
+      format.html { redirect_to store_url, notice: 'Your cart is currently empty' }
+      format.xml { head :ok }
+      #format.json { head :no_content }
     end
   end
 
